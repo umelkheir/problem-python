f0cf920ee2146d0e7ed014bb8bb572771c297edd
umelkheir <95570697+umelkheir@users.noreply.github.com>
2023-02-19 16:08:29 +0200

    Update problem.py

--- problem.py
+++ problem.py
@@ -1,10 +1,12 @@
 def convert_dollars(dollars):
     message_one =" Dollars are "
     message_two = " Euros"
+    ##One of the errors is this result line below 
     result = int((dollars/1.06))
     return str((dollars))  + message_one + str(result) + message_two
 
 user_input = input("How many dollars would you like to convert: ")
+##The error was in this dollars_result line below 
 dollars_result = int(convert_dollars(user_input)) 
 
 if float(user_input) > 50:
