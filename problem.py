2930c5da16a45ee1e71277f33a12407dc071f8a7
umelkheir <95570697+umelkheir@users.noreply.github.com>
2023-02-19 16:06:28 +0200

    Create problem.py

---
+++ problem.py
@@ -0,0 +1,13 @@
+def convert_dollars(dollars):
+    message_one =" Dollars are "
+    message_two = " Euros"
+    result = int((dollars/1.06))
+    return str((dollars))  + message_one + str(result) + message_two
+
+user_input = input("How many dollars would you like to convert: ")
+dollars_result = int(convert_dollars(user_input)) 
+
+if float(user_input) > 50:
+    print("Wow that is a lot of money")
+
+print(dollars_result)
